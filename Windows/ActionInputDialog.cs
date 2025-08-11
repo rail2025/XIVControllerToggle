5dfaf00ca341d1a827a991108be8ca06a2cbf225
rail2025 <undrln@gmail.com>
2025-08-11 16:19:25 -0700

    Update ActionInputDialog.cs

--- Windows/ActionInputDialog.cs
+++ Windows/ActionInputDialog.cs
@@ -6,7 +6,7 @@ using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
 using Dalamud.Interface.Windowing;
 
-using ImGuiNET;
+using Dalamud.Bindings.ImGui;
 
 using System;
 using System.Collections.Generic;
